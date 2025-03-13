9689317682373a1631a9d45c99e49071dc6e49a1
Derek Verduijn <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2025-03-13 13:42:33 +0000

    First settings implemented

--- FinnishLanguageTrainer/src/lib/Settings.svelte
+++ FinnishLanguageTrainer/src/lib/Settings.svelte
@@ -1,18 +1,67 @@
 <script lang="ts">
-  let setLanguage: string = "finnish";
+  // Language
+  let setLanguage: string = "Finnish";
 
   const setEnglish = () => {
-    setLanguage = "english";
+    setLanguage = "English";
   };
   const setFinnish = () => {
-    setLanguage = "finnish";
-  }
+    setLanguage = "Finnish";
+  };
+
+  // Wordtype
+
+  // Amount of words
+  let wordAmount: number = 6;
+
+  const AddOne = () => {
+    wordAmount += 1;
+  };
+  const SubtractOne = () => {
+    wordAmount -= 1;
+    if (wordAmount === 0) {
+      wordAmount = 1;
+    }
+  };
+
+  // Random
+  let random: boolean = true
+  let randomText: string = ""
+  const setRandomTrue = () => {
+    random = true;
+    randomText = ""
+  };
+  const setRandomFalse = () => {
+    random = false;
+    randomText = "not ";
+  };
 </script>
 
-<p>
+<h2>Settings</h2>
+
+<h3 class="nob">Language</h3>
+<p class="not">
   Click on the language that you want to practise:
   <button onclick={setEnglish}>English</button>
   <button onclick={setFinnish}>Finnish</button>
-  <br>
-  Current Language is {setLanguage}
+  <br />
+  The current language is {setLanguage}
+</p>
+
+<h3 class="nob">Word Amount</h3>
+<p class="not">
+  Give the amount of words you would like to practise:
+  <button onclick={SubtractOne}>Subtract one</button>
+  <button onclick={AddOne}>Add one</button>
+  <br />
+  The current word amount is {wordAmount}
+</p>
+
+<h3 class="nob">Random Order</h3>
+<p class="not">
+  Would you like to randomise the word order?
+  <button onclick={setRandomTrue}>Yes</button>
+  <button onclick={setRandomFalse}>No</button>
+  <br />
+  The word order is {randomText}randomised.
 </p>
